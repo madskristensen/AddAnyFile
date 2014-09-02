16c6a57923b2b66aacd97dcfc92c4391604b4a04
Mads Kristensen <post@madskristensen.net>
2014-09-01 20:29:22 -0700

    Initial commit

---
+++ AddAnyFile/AddAnyFilePackage.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualBasic;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace MadsKristensen.AddAnyFile
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(GuidList.guidAddAnyFilePkgString)]
+    public sealed class AddAnyFilePackage : ExtensionPointPackage
+    {
+        private static DTE2 _dte;
+
+        protected override void Initialize()
+        {
+            _dte = GetService(typeof(DTE)) as DTE2;
+            base.Initialize();
+
+            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (null != mcs)
+            {
+                CommandID menuCommandID = new CommandID(GuidList.guidAddAnyFileCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
+                MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                mcs.AddCommand(menuItem);
+            }
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            UIHierarchyItem item = GetSelectedItem();
+
+            if (item == null)
+                return;
+
+            string folder = FindFolder(item);
+            string input = Interaction.InputBox("Please enter a file name", "File name", "file.txt");
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                string path = Path.Combine(folder, input);
+
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, string.Empty);
+
+                    _dte.ItemOperations.OpenFile(path);
+                    ProjectItem projectItem = AddFileToActiveProject(path);
+                    // TODO: Select the new file in Solution Explorer. Help wanted.
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("The file '" + path + "' already exist.");
+                }
+            }
+        }
+
+        private static string FindFolder(UIHierarchyItem item)
+        {
+            string folder = null;
+
+            ProjectItem projectItem = item.Object as ProjectItem;
+            Project project = item.Object as Project;
+
+            if (projectItem != null)
+            {
+                string fileName = projectItem.FileNames[0];
+
+                if (File.Exists(fileName))
+                {
+                    folder = Path.GetDirectoryName(fileName);
+                }
+                else
+                {
+                    folder = fileName;
+                }
+            }
+            else if (project != null)
+            {
+                Property prop = project.Properties.Item("FullPath");
+                if (prop != null)
+                {
+                    string value = prop.Value.ToString();
+
+                    if (File.Exists(value))
+                    {
+                        folder = Path.GetDirectoryName(value);
+                    }
+                    else if (Directory.Exists(value))
+                    {
+                        folder = value;
+                    }
+                }
+            }
+            return folder;
+        }
+
+        public static UIHierarchyItem GetSelectedItem()
+        {
+            var items = (Array)_dte.ToolWindows.SolutionExplorer.SelectedItems;
+
+            foreach (UIHierarchyItem selItem in items)
+            {
+                return selItem;
+            }
+
+            return null;
+        }
+
+        public static ProjectItem AddFileToActiveProject(string fileName)
+        {
+            Project project = GetActiveProject();
+
+            if (project == null)
+                return null;
+
+            string projectFilePath = project.Properties.Item("FullPath").Value.ToString();
+            string projectDirPath = Path.GetDirectoryName(projectFilePath);
+
+            if (!fileName.StartsWith(projectDirPath, StringComparison.OrdinalIgnoreCase))
+                return null;
+
+            return project.ProjectItems.AddFromFile(fileName);
+        }
+
+        public static Project GetActiveProject()
+        {
+            try
+            {
+                Array activeSolutionProjects = _dte.ActiveSolutionProjects as Array;
+
+                if (activeSolutionProjects != null && activeSolutionProjects.Length > 0)
+                    return activeSolutionProjects.GetValue(0) as Project;
+            }
+            catch (Exception)
+            {
+            }
+
+            return null;
+        }
+    }
+}
