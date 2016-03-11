720f2a4ac68f854d380f352153f7bd97081e36c1
Mads Kristensen <post@madskristensen.net>
2016-03-11 11:12:54 -0800

    v.3.0

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -5,7 +5,7 @@ namespace MadsKristensen.AddAnyFile
 		public const string Id = "2E78AA18-E864-4FBB-B8C8-6186FC865DB3";
 		public const string Name = "Add New File";
 		public const string Description = "The fastest and easiest way to add new files to any project - including files that start with a dot";
-		public const string Version = "2.4";
+		public const string Version = "3.0";
 		public const string Author = "Mads Kristensen";
 		public const string Tags = "file, add";
 	}
